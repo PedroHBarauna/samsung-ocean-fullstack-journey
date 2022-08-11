6bcfefcafc3369acc426f1a5801e01f32980265a
PedroHBarauna <barauna38@gmail.com>
2022-08-11 11:08:37 -0300

    adicionando uma lista de objetos e adicionando o nodemon

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,31 @@ const app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World');
-})
+});
 
 app.get('/oi', function(req, res){
     res.send('Olá Mundo');
-})
+});
+
+// Nosso backend armazena as pontuaçÕes jogadas
+//criar lista com as pontuações
+
+const lista = [
+  {
+    "id": 1,
+    "nome": "Pedro",
+    "pontos": 90,
+  },
+  {
+    "id": 2,
+    "nome": "Paulo",
+    "pontos": 120,
+  },
+  {
+    "id": 3,
+    "nome": "Cátia",
+    "pontos": 50,
+  },
+];
 
 app.listen(3000);
