140afb528f931c8210a893763bc484c3256994a8
PedroHBarauna <barauna38@gmail.com>
2022-08-22 12:00:09 -0300

    adicionando a imagem e criando o componente do jogo

---
+++ frontend/src/components/Jogo/Jogo.js
@@ -0,0 +1,10 @@
+import "./Jogo.css"
+import clouds from "../../assets/clouds.png"
+
+function Jogo(){
+    return <div className="jogo">
+        <img src={clouds}></img>
+    </div>
+}
+
+export default Jogo;
