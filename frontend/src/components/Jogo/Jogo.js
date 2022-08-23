ac00630d27cce7464ff72b3dc1aac4e48fe854c1
PedroHBarauna <barauna38@gmail.com>
2022-08-23 11:13:52 -0300

    setando o use state

--- frontend/src/components/Jogo/Jogo.js
+++ frontend/src/components/Jogo/Jogo.js
@@ -5,11 +5,17 @@ import cano from "../../assets/pipe.png"
 import { useState } from "react"
 
 function Jogo() {
+    console.log("Componente de Jogo Renderizado");
+
     const [estaPulando, setEstaPulando] = useState(false);
 
     document.onkeydown = function(){
-        
+        console.log("On Key Down")
+
+        setEstaPulando(true);
     }
+
+    console.log(18, { estaPulando });
 
     return (
     <div className="jogo">
