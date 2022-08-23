1efd06bec3e9b09c171f835a594b1179e26f5ea9
PedroHBarauna <barauna38@gmail.com>
2022-08-23 10:42:59 -0300

    adicionando as animações e iniciando o processo de criação da função de apertar

--- frontend/src/components/Jogo/Jogo.js
+++ frontend/src/components/Jogo/Jogo.js
@@ -2,14 +2,23 @@ import "./Jogo.css"
 import clouds from "../../assets/clouds.png"
 import mario from "../../assets/mario.gif"
 import cano from "../../assets/pipe.png"
+import { useState } from "react"
 
-function Jogo(){
-    return <div className="jogo">
+function Jogo() {
+    const [estaPulando, setEstaPulando] = useState(false);
+
+    document.onkeydown = function(){
+        
+    }
+
+    return (
+    <div className="jogo">
         <img className="nuvens" src={clouds}  alt="Nuvens"></img>
         <img className="mario" src={mario} alt="Mario"></img>
-        <img className="cano" src={cano}></img>
+        <img className="cano" src={cano} alt="Cano"></img>
         <div className="chao"></div>
-    </div>
+    </div>      
+    );
 }
 
 export default Jogo;
