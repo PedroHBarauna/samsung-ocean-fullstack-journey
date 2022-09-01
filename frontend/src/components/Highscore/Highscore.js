cd2c55e6f90316e03cd52d3643cc6842df07ab2b
PedroHBarauna <barauna38@gmail.com>
2022-09-01 12:16:45 -0300

    mostrando as pontuacoes e salvando elas

--- frontend/src/components/Highscore/Highscore.js
+++ frontend/src/components/Highscore/Highscore.js
@@ -1,8 +1,40 @@
+import { useEffect, useState } from "react";
 import "./Highscore.css"
 
 function Highscore(props){
 
-    fetch("http://localhost:3333/pontuacoes").then(console.log)
+    const [itens, setItens] = useState(undefined);
+
+    useEffect(function(){
+
+        async function carregarPontuacoes(){
+            const response = await fetch("http://localhost:3333/pontuacoes");
+            const body = await response.json();
+
+            setItens(body);
+        }
+        
+        carregarPontuacoes();
+    }, []);
+
+    const itensEstaoCarregando = itens === undefined;
+
+    async function salvarPontuacoes(event){
+        const form =event.target;
+        const name = form.name.value;
+
+        const response = await fetch("http://localhost:3333/pontuacoes", {
+            method: "POST",
+            body: JSON.stringify({name:name, pontos: props.pontos}),
+            headers:{
+                "Content-type": "application/json",
+            },
+        });
+
+        const body = await response.json();
+        
+        console.log("Pontuação salva:", body)
+    }
 
     return( 
         <div className="highscore">
@@ -10,15 +42,24 @@ function Highscore(props){
             <div>
                 <h1>Highscore</h1>
 
-                <div>Paulo - 90 pontos</div>
-                <div>Paulo - 90 pontos</div>
-                <div>Paulo - 90 pontos</div>
+                {itensEstaoCarregando ? (
+                    <div>Carregando...</div>
+                ) : (
+                    <div>  
+                    {itens.map((item, index)=>(
+                        <div key={`score_${index}`}>
+                            {item.name} - {item.pontos}
+                        </div>
+                    ))}
+                    </div>
+                )}
+
             </div>
 
             <div>
                 <h1>Registre sua pontuação</h1>
-                <form>
-                    <input type="text" pattern="^[0-9]*$" placeholder="Digite o seu nome..." />
+                <form onSubmit={salvarPontuacoes}>
+                    <input type="text" name="name" placeholder="Digite o seu nome..." />
                     <input type="submit" placeholder="Enviar"/>
                 </form>
             </div>
